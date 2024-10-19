ec3493c1a95f0237e5a6215ca1f22acb03fba6cd
PUGALAURA2000 <carlitos22221@hotmail.com>
2024-10-19 16:54:47 -0500

    commit

--- src/test/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplicationTests.java
+++ src/test/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bolsadeideas.springboot.app;
+/*package com.bolsadeideas.springboot.app;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class SpringBootDataJpaApplicationTests {
 	}
 
 }
+*/
